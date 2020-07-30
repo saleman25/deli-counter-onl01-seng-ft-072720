410f44dbcebee2ee0198154e37172ea1256a7388
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 03:08:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 katz_deli = []
 
-def line(array)
-  if array.length == 0
-    return "The line is currently empty."
-  else 
- array.each_with_index do |name, index|
-      new_array.push("#{index + 1}. #{name}")
-    end 
+def line(katz_deli)
+  if katz_deli.length == 3
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  elsif katz_deli.length > 3
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]} 4. #{katz_deli[3]} 5. #{katz_deli[4]} 6. #{katz_deli[5]} 7. #{katz_deli[6]} 8. #{katz_deli[7]} 9. #{katz_deli[8]} 10. #{katz_deli[9]}"
+  else
+    puts "The line is currently empty."
+  end
+end
 
