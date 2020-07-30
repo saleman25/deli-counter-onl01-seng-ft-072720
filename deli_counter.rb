22a2e3486249ca2583ffd1088e7a21813edbbc1e
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 03:05:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if !array.length
+    return "The line is currently empty."
+  else 
+ array.each_with_index do |name, index|
+      new_array.push("#{index + 1}. #{name}")
+    end 
